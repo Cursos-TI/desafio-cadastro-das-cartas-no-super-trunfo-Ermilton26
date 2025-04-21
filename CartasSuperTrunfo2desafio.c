e1500e3ce6ddc77053a3637440d33487ad61ddff
Ermilton Analista de Sistemas <ermiltoncezar@gmail.com>
2025-04-21 16:03:11 +0000

    terceiro desafio concluido

--- CartasSuperTrunfo2desafio.c
+++ CartasSuperTrunfo2desafio.c
@@ -71,6 +71,14 @@ int main () {
     densidade2 = populacao02 / area02;
     pibpercapita2 = pib02 / populacao02;
 
+    float superpoder1, superpoder2;
+    
+    
+    superpoder1 = (populacao + area + pib + pontosturisticos + pibpercapita + 
+        (1/densidade));
+    
+    superpoder2 = (populacao02 + area02 + pib02 + pontosturisticos02 + pibpercapita2 + 
+                   (1/densidade2));
 
     //Impressão na tela da carta 01
     //Utilização do printf para tal tarefa
@@ -87,6 +95,7 @@ int main () {
     printf("pontos turisticos : %d \n", pontosturisticos);
     printf("densidade populacional : %.2f hab/km² \n", densidade);
     printf("PIB per Capita : %.2f reais \n", pibpercapita*1000000000);
+    printf("SUPER PODER CARTA 1 : %.2f \n", superpoder1);
 
     //Impressão na tela da carta 02
     //Utilização do printf para tal tarefa;
@@ -103,6 +112,70 @@ int main () {
     printf("pontos turisticos : %d \n", pontosturisticos02);
     printf("densidade populacional : %.2f hab/km² \n", densidade2);
     printf("PIB per Capita : %.2f reais \n", pibpercapita2*1000000000);
+    printf("SUPER PODER CARTA 2 : %.2f \n", superpoder2);
+
+/*  Comparação de Cartas:
+
+    População: Carta 1 venceu (1)
+    Área : Carta 1 venceu (1)
+    PIB: Carta 1 venceu (1)
+    Pontos Turísticos: Carta 1 venceu (1)
+    Densidade Populacional: Carta 2 venceu (0)
+    PIB per Capita: Carta 1 venceu (1)
+    Super Poder: Carta 1 venceu (1)*/
+
+    printf("\n");
+    printf("******COMPARAÇÃO DE CARTAS*****\n");
+    printf("\n");
+
+    if (populacao > populacao02) {  // Verifica se valor1 é maior que valor2
+        printf("População : Carta 01 venceu! (1) \n");
+      } else {
+        printf("População : Carta 02 venceu! (0)\n");
+      }
+
+      if (area > area02) {  // Verifica se valor1 é maior que valor2
+        printf("Area : Carta 01 venceu! (1) \n");
+      } else {
+        printf("Area : Carta 02 venceu! (0)\n");
+      }
+    
+      if (pib > pib02) {  // Verifica se valor1 é maior que valor2
+        printf("PIB : Carta 01 venceu! (1) \n");
+      } else {
+        printf("PIB : Carta 02 venceu! (0)\n");
+      }
+      if (pontosturisticos > pontosturisticos02) {  // Verifica se valor1 é maior que valor2
+        printf("Pontos turisticos : Carta 01 venceu! (1) \n");
+      } else {
+        printf("Pontos turisticos : Carta 02 venceu! (0)\n");
+      }
+      
+      if (densidade2 > densidade) {  // Verifica se valor1 é maior que valor2
+        printf("Densidade : Carta 01 venceu! (1) \n");
+      } else {
+        printf("Densidade : Carta 02 venceu! (0)\n");
+      }
+
+      if (pib > pib02) {  // Verifica se valor1 é maior que valor2
+        printf("PIB : Carta 01 venceu! (1) \n");
+      } else {
+        printf("PIB : Carta 02 venceu! (0)\n");
+      }
+
+      if (pibpercapita > pibpercapita2) {  // Verifica se valor1 é maior que valor2
+        printf("PIB percapita : Carta 01 venceu! (1) \n");
+      } else {
+        printf("PIB percapita : Carta 02 venceu! (0)\n");
+      }
+
+      if (superpoder1 > superpoder2) {  // Verifica se valor1 é maior que valor2
+        printf("Super poder : Carta 01 venceu! (1) \n");
+      } else {
+        printf("Super Poder : Carta 02 venceu! (0)\n");
+      }
+    
+    
 
     return 0;
 }
